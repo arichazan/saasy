c5212bf2b39a870600b88ddbabd861d3c80698ac
Ari Chazan <ari@itbcn.com>
2015-11-18 10:14:31 +0100

    init

---
+++ test-recurly.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import recurly
+
+recurly.SUBDOMAIN = 'vreasy'
+recurly.API_KEY = '591e4223895e4e058280915e36db7785'
+
+# Set a default currency for your API requests
+recurly.DEFAULT_CURRENCY = 'EUR'
